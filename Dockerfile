15f7a5e3e16c3a4a27ee9596ad740ba5d3fffb5e
Lamiaa Khaled <lamiakhaled167@gmail.com>
2025-07-04 15:56:33 +0300

    Remove all files

--- Dockerfile
+++
@@ -1,5 +0,0 @@
-FROM openjdk:17-jdk-alpine
-WORKDIR /exam-app
-COPY target/exam-0.0.1-SNAPSHOT.jar exam.jar
-EXPOSE 8083
-ENTRYPOINT ["java", "-jar", "exam.jar"]
--- src/main/java/com/university/exam/examManagement/repos/QuestionPoolRepository.java
+++
@@ -1,8 +0,0 @@
-package com.university.exam.examManagement.repos;
-
-import com.university.exam.examManagement.entities.QuestionPool;
-import org.springframework.data.jpa.repository.JpaRepository;
-import java.util.UUID;
-
-public interface QuestionPoolRepository extends JpaRepository<QuestionPool, UUID> {
-} 
